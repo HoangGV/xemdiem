2fa04421c146786a3309edb8873a919717ef1a7c
Nguyễn Đức Hoàng <104571272+HoangGV@users.noreply.github.com>
2026-08-17 23:33:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def fetch_student_data(masv: str, password: str):
 
 
 # ================= GIAO DIỆN =================
-st.title("🎓 Tra cứu KQHT HUEdi")
+st.title("🎓 Tra cứu KQHT HUEdu")
 st.caption("Dùng khi web chính lag!!")
 
 with st.form("form_tra_cuu"):
