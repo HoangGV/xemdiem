3eff05cb71c66c7db437243ac18dcf3ccd0c5301
Nguyễn Đức Hoàng <104571272+HoangGV@users.noreply.github.com>
2026-08-18 10:04:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown(
     <meta property="og:type" content="website">
     """,
     unsafe_allow_html=True
-
+)
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
